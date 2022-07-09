2ece8757dad5bc3e7043ec4dcc48f62a00f5f89a
SC1314520 <104412070+SC1314520@users.noreply.github.com>
2022-07-09 15:47:19 +0800

    s

---
+++ js/shopping.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+    $('.A1').click(function(event) {
+        $('.M1').show();
+        $('.M2').hide();
+        $('.M3').hide();
+        $('.M4').hide();
+        $('.M5').hide();
+        $('.M6').hide();
+        $('.M7').hide();
+        $('.M8').hide();
+    });
+    $('.A2').click(function(event) {
+        $('.M1').hide();
+        $('.M2').show();
+        $('.M3').hide();
+        $('.M4').hide();
+        $('.M5').hide();
+        $('.M6').hide();
+        $('.M7').hide();
+        $('.M8').hide();
+      });
+      $('.A3').click(function(event) {
+        $('.M1').hide();
+        $('.M2').hide();
+        $('.M3').show();
+        $('.M4').hide();
+        $('.M5').hide();
+        $('.M6').hide();
+        $('.M7').hide();
+        $('.M8').hide();
+      });
+      $('.A4').click(function(event) {
+        $('.M1').hide();
+        $('.M2').hide();
+        $('.M3').hide();
+        $('.M4').show();
+        $('.M5').hide();
+        $('.M6').hide();
+        $('.M7').hide();
+        $('.M8').hide();
+      });
+      $('.A5').click(function(event) {
+        $('.M1').hide();
+        $('.M2').hide();
+        $('.M3').hide();
+        $('.M4').hide();
+        $('.M5').show();
+        $('.M6').hide();
+        $('.M7').hide();
+        $('.M8').hide();
+      });
+      $('.A6').click(function(event) {
+        $('.M1').hide();
+        $('.M2').hide();
+        $('.M3').hide();
+        $('.M4').hide();
+        $('.M5').hide();
+        $('.M6').show();
+        $('.M7').hide();
+        $('.M8').hide();
+      });
+      $('.A7').click(function(event) {
+        $('.M1').hide();
+        $('.M2').hide();
+        $('.M3').hide();
+        $('.M4').hide();
+        $('.M5').hide();
+        $('.M6').hide();
+        $('.M7').show();
+        $('.M8').hide();
+      });
+      $('.A8').click(function(event) {
+        $('.M1').hide();
+        $('.M2').hide();
+        $('.M3').hide();
+        $('.M4').hide();
+        $('.M5').hide();
+        $('.M6').hide();
+        $('.M7').hide();
+        $('.M8').show();
+      });
+  });
